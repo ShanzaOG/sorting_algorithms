91ff11b3a682a09dff40207f1cb51d75e6f8ef54
ShanzaOg <allanshanza@gmail.com>
2023-06-26 08:04:02 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,29 @@
 #include "sort.h"
 
 /**
-  * shell_sort - A function tat sorts an array usin shell algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
-void shell_sort(int *array, size_t size)
-{
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
-
-	if (array == NULL || size < 2)
-		return;
-
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
-	{
-		for (i = gap; i < size; i++)
-		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
-
-		}
-		print_array(array, size);
-	}
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+ 
+ void shell_sort(int *array, size_t size) {
+    int interval = 1;
+    while (interval <= size / 3) {
+        interval = interval * 3 + 1;
+    }
+    while (interval > 0) {
+        for (int a = interval; a < size; a++) {
+            int temp = array[a];
+            int j;
+            for (j = a; j >= interval && array[j - interval] > temp; j -= interval) {
+                array[j] = array[j - interval];
+            }
+            array[j] = temp;
+        }
+        interval = (interval - 1) / 3;
+       print_array(array, size);
+    }
 }
